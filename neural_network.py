0adb975bbc6645193ced554f0dd7df8c6973c6ba
Bas Boot <github@basboot.nl>
2025-06-10 22:05:46 +0200

    Increase complexity of network (might be overfitting!)

--- neural_network.py
+++ neural_network.py
@@ -1,14 +1,17 @@
 import torch
 
 # TODO: tune NN
+# TODO: normalize input/output
 class NeuralNetwork(torch.nn.Module):
     def __init__(self, input_size, output_size):
         super(NeuralNetwork, self).__init__()
-        self.fc1 = torch.nn.Linear(input_size, 64)
+        self.fc1 = torch.nn.Linear(input_size, 128)
         self.relu1 = torch.nn.ReLU()
-        self.fc2 = torch.nn.Linear(64, 32)
+        self.fc2 = torch.nn.Linear(128, 64)
         self.relu2 = torch.nn.ReLU()
-        self.fc3 = torch.nn.Linear(32, output_size)
+        self.fc3 = torch.nn.Linear(64, 32)
+        self.relu3 = torch.nn.ReLU()  
+        self.fc4 = torch.nn.Linear(32, output_size)
 
     def forward(self, x):
         x = self.fc1(x)
@@ -16,4 +19,6 @@ class NeuralNetwork(torch.nn.Module):
         x = self.fc2(x)
         x = self.relu2(x)
         x = self.fc3(x)
+        x = self.relu3(x)  
+        x = self.fc4(x)
         return x
