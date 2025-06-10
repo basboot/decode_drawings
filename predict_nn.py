45bc30af3544b98ea3cbd27236194f75e13653d6
Bas Boot <github@basboot.nl>
2025-06-10 22:03:18 +0200

    Normalize data

--- predict_nn.py
+++ predict_nn.py
@@ -22,8 +22,8 @@ print("PyTorch version:", torch.__version__)
 device = mps_device if torch.backends.mps.is_available() else torch.device("cpu")
 print(f"Using device: {device}")
 
-VIDEO = "6" # video to predict on
-MODEL = "19"  # video the model was trained on
+VIDEO = "2" # video to predict on
+MODEL = "1"  # video the model was trained on
 
 
 # Determine device
@@ -36,10 +36,19 @@ else:
 
 
 ball_information, video_information = get_video_data(VIDEO)
-
 ball_information = np.array(ball_information)
+
+# Adjust angles in ball_information to be within [-90, 90]
+ball_information[:, :, 4] = (ball_information[:, :, 4] + 180) % 180
+ball_information[:, :, 4] = np.where(ball_information[:, :, 4] > 90, ball_information[:, :, 4] - 180, ball_information[:, :, 4])
+
+
 # Flatten ball_information: 3x5 => 15
 ball_information_flat = ball_information.reshape(ball_information.shape[0], -1)
+
+# norm
+ball_information_flat += [-1269/2, -719/2, -105, -105, 0] * 3
+ball_information_flat /= [1269/2, 719/2, 105, 105, 90] * 3
 
 
 # Convert to np arrays to pt tensor, and put on gpu
@@ -70,6 +79,11 @@ with torch.no_grad(): # gradient not needed for eval
     predictions_tensor = loaded_model(X_predict_tensor)
     coords = predictions_tensor.cpu().numpy() 
 
+    # de-norm
+    coords *= np.array([11, 1, 11])
+    coords -= np.array([0, 0, -18])
+
+
 # filter audio
 video_information['volume'] = butter_lowpass_filter(video_information['volume'], 1, 60, 2)
 
@@ -93,7 +107,7 @@ plot_configs = [
     {
             "x_data": drawing_x, "y_data": drawing_y,
             "xlabel": 'X', "ylabel": 'Z', "title": 'Drawing',
-            "marker": 'o', "marker_size": 10, "color": "k",  "equal_axis": True, "scatter_only": True, "mirror_y": True
+            "marker": 'o', "marker_size": 1, "color": "k",  "equal_axis": True, "scatter_only": True, "mirror_y": True
         },
 ]
 
--- train_nn.py
+++ train_nn.py
@@ -26,15 +26,49 @@ device = mps_device if torch.backends.mps.is_available() else torch.device("cpu"
 print(f"Using device: {device}")
 
 
-VIDEO = "19"
+# TODO: create option to combine multiple datasets
+
+VIDEO = "1"
+
+# 22 is slechter dan 21
 
 ball_information, video_information, location_information = get_video_data(VIDEO, createTrainingData=True)
 
+# ball_information2, video_information2, location_information2 = get_video_data("22", createTrainingData=True)
+
 ball_information = np.array(ball_information)
+# ball_information2 = np.array(ball_information2)
+location_information = np.array(location_information)
+# location_information2 = np.array(location_information2)
+
+# Combine ball and video information from both datasets
+# ball_information = np.concatenate((ball_information, ball_information2), axis=0)
+# location_information = np.concatenate((location_information, location_information2), axis=0)
+
+# Adjust angles in ball_information to be within [-90, 90]
+ball_information[:, :, 4] = (ball_information[:, :, 4] + 180) % 180
+ball_information[:, :, 4] = np.where(ball_information[:, :, 4] > 90, ball_information[:, :, 4] - 180, ball_information[:, :, 4])
+
 # Flatten ball_information: 3x5 => 15
 ball_information_flat = ball_information.reshape(ball_information.shape[0], -1)
 # Location already flat: 3
 location_information = np.array(location_information)
+
+
+# add noise
+# Add different noise levels to different columns/features
+noise_std = [5, 5, 8, 8, 10] * 3  # Repeat for 3 ball detections
+noise = np.random.normal(0, noise_std, ball_information_flat.shape)
+ball_information_flat_noisy = ball_information_flat + noise
+
+# in: x, y, w, h, angle
+# out: x, y, z 18 +- 11, 0 - 1, 18 +- 11
+
+ball_information_flat += [-1269/2, -719/2, -105, -105, 0] * 3
+ball_information_flat /= [1269/2, 719/2, 105, 105, 90] * 3
+location_information += np.array([0, 0, -18]) 
+location_information /= np.array([11, 1, 11])
+
 
 # Convert to np arrays to pt tensors
 X_tensor = torch.tensor(ball_information_flat, dtype=torch.float32) 
@@ -53,7 +87,7 @@ y_train, y_test = y_train.to(device), y_test.to(device)
 train_dataset = TensorDataset(X_train, y_train)
 test_dataset = TensorDataset(X_test, y_test)
 
-batch_size = 32 # TODO: tune
+batch_size = 256 # TODO: tune
 train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False) # No need to shuffle test data
 
@@ -67,7 +101,7 @@ criterion = torch.nn.MSELoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
 # train NN
-num_epochs = 200 # TODO: tune
+num_epochs = 200 # TODO: tune => experiment with less epochs, loss is 0.0000 after 20 epochs! 15-20 seems enough when normalizing and training with enough data
 for epoch in range(num_epochs):
     model.train() # set model to training mode
     epoch_loss = 0
@@ -82,8 +116,8 @@ for epoch in range(num_epochs):
         optimizer.step()
         epoch_loss += loss.item()
 
-    if (epoch + 1) % 10 == 0:
-        print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {epoch_loss/len(train_dataloader):.4f}')
+    if (epoch + 1) % 1 == 0:
+        print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {epoch_loss/len(train_dataloader):.8f}')
 
 print("Training finished.")
 
